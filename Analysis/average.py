0d2f2c7ca9c9cf2894efe1d34e8dd91b80981bf8
Kun Feng <fkengun@gmail.com>
2019-02-12 22:48:46 +0000

    Add average script

---
+++ Analysis/average.py
@@ -0,0 +1,42 @@
+import numpy as np
+import argparse
+import sys
+
+def arg_parse():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f', '--file', dest = 'data_file', required = True, help = 'input raw data in one column')
+  parser.add_argument('-d', '--dimension', dest = 'dims', nargs = '+', type = int, required = True, help = 'dimension of data array')
+  parser.add_argument('-x', '--axis', dest = 'ave_axis', type = int, required = True, help = 'the dimension along which to average')
+  args = parser.parse_args()
+  return args.data_file, args.dims, args.ave_axis
+
+if __name__== "__main__":
+  data_file, dims, ave_axis = arg_parse();
+
+  with open(data_file) as f:
+    data = f.read().split('\n')
+
+  del data[-1]
+  data = [float(x) for x in data]
+  print "Length of data: " + str(len(data))
+  data = np.array(data)
+  shape = dims
+  data_mat = data.reshape(shape)
+  ave = np.mean(data_mat, axis = ave_axis)
+  stddev = np.std(data_mat, axis = ave_axis)
+
+  print "Average:"
+  if len(ave.shape) > 2:
+    for n in ave:
+      np.savetxt(sys.stdout, n, fmt = '%.7f')
+      print ''
+  else:
+    np.savetxt(sys.stdout, ave, fmt = '%.7f')
+
+  print "Standard deviation:"
+  if len(ave.shape) > 2:
+    for n in stddev:
+      np.savetxt(sys.stdout, n, fmt = '%-6s')
+      print ''
+  else:
+    np.savetxt(sys.stdout, ave, fmt = '%-6s')
