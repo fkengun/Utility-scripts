b1a2860508fdb75628decd9524376276babbcd7f
Kun Feng <fkengun@gmail.com>
2019-10-03 14:52:58 -0500

    Minor change

--- OrangeFS/env_ares.sh
+++ OrangeFS/env_ares.sh
@@ -7,6 +7,7 @@ then
 fi
 mpssh > /dev/null 2>&1 || { echo >&2 "mpssh is not found.  Aborting."; exit 1; }
 
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PVFS2_HOME="/opt/ohpc/pub/orangefs"
 PVFS2_SRC_HOME=""
 SERVER_LOCAL_PATH="/mnt/hdd/kfeng"
@@ -18,7 +19,6 @@ PVFS2TAB_FILE_SERVER="/mnt/hdd/kfeng/pvfs2tab"
 PARENT_DIR=$PVFS2_SRC_HOME
 SCRIPT_DIR="OrangeFS_scripts"
 MOUNT_POINT=$CLIENT_LOCAL_PATH"/pvfs2-mount"
-CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-servers=`awk '{printf("%s,",$1)}' servers`
-number=`awk 'END{print NR}' servers`
+servers=`awk '{printf("%s,",$1)}' ${CWD}/servers`
+number=`awk 'END{print NR}' ${CWD}/servers`
 hs_hostname_suffix="-40g"
