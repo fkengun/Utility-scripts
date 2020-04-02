c9d8e864dbb1e996282a4ac51d8b3b847408a9af
Kun Feng <fkengun@gmail.com>
2020-04-02 18:13:58 -0500

    Add script to remove all data in MongoDB

---
+++ MongoDB/removeall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -f ${CWD}/env.sh ]
+then
+  source ${CWD}/env.sh
+else
+  echo "env.sh does not exist, quiting ..."
+  exit
+fi
+
+mongo --host ${CWD}/clients -p ${MONGO_PORT} < clear_db.js
