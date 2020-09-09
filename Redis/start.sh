9d53b55c37eda87f8b54a7245e532efb9874b725
Kun Feng <fkengun@gmail.com>
2020-09-08 23:22:36 -0500

    Disable replica for Redis 4.x/5.x

--- Redis/start.sh
+++ Redis/start.sh
@@ -38,7 +38,7 @@ do
   echo "cluster-config-file nodes.conf" >> $port/$CONF_FILE
   echo "cluster-node-timeout 5000" >> $port/$CONF_FILE
   echo "appendonly yes" >> $port/$CONF_FILE
-  #echo "appendfsync always" >> $port/$CONF_FILE
+  echo "appendfsync always" >> $port/$CONF_FILE
   echo "protected-mode no" >> $port/$CONF_FILE
   echo "logfile $LOCAL_DIR/$port/file.log" >> $port/$CONF_FILE
   ((i=i+1))
@@ -95,7 +95,7 @@ do
 done
 if version_gt $REDIS_VER "5.0"
 then
-  cmd="${cmd}--cluster-replicas 1"
+  cmd="${cmd}--cluster-replicas 0"
 fi
 echo yes | $cmd
 
