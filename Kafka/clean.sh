4e154a6ae4727ef933bb65e1b15c0ce1937ce7de
Kun Feng <fkengun@gmail.com>
2021-01-04 14:32:00 -0600

    OCD about braces

---
+++ Kafka/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -f ${CWD}/env.sh ]
+then
+  source ${CWD}/env.sh
+else
+  echo "env.sh does not exist, quiting ..."
+  exit
+fi
+
+mpssh -f ${ZOOKEEPER_SERVERS_HOSTFILE} "killall -9 java"
+mpssh -f ${ZOOKEEPER_SERVERS_HOSTFILE} "rm -rf ${ZOOKEEPER_DATA_DIR}/*"
+mpssh -f ${KAFKA_SERVERS_HOSTFILE} "rm -rf ${KAFKA_LOG_DIR}/*"
